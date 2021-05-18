07a29ed81931410406356ca676bc3e2a85277222
admin <admin@DESKTOP-OTC4CTU>
2021-05-18 21:50:56 +0700

    Pertemuan Ke-2

--- PBOPert-2/app/models/Person.java
+++ PBOPert-2/app/models/Person.java
@@ -1,5 +1,32 @@
 package models;
 
 public class Person {
-
+  public String nama;
+  public int noiden;
+  private int nohp;
+  private int norek;
+  
+  //ciptakan konstruktor
+  public Person(String nama, int noiden, int nohp, int norek){
+	  this.nama ="Ari Fadli";
+	  this.noiden = 123456;
+	  this.nohp = 67890;
+	  this.norek = 123098;
+  }
+  
+  public String getnama(){
+	  return nama;
+  }
+ 
+  public int getnoiden(){
+	  return noiden;
+  }
+  
+  public void setnama(String name){
+	  nama = name;
+  }
+  
+  public void setnoiden(int nomor){
+	  noiden = nomor;
+  }
 }
