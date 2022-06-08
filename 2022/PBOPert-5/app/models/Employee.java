1d34c0f6c75da4e75a0ea200e7f4f3fec935263f
arifadli <fadli.te.unsoed@gmail.com>
2022-06-08 09:09:38 +0700

    Pertemuan 7

---
+++ 2022/PBOPert-5/app/models/Employee.java
@@ -0,0 +1,18 @@
+package models;
+
+import javax.persistence.Entity;
+
+import play.db.jpa.Model;
+
+@Entity
+public class Employee extends Model {
+	public String namadepan;
+	public String namabelakang;
+	public int noidentitas;
+	
+	public String toString(){
+		return this.namadepan;
+	}
+	
+
+}
